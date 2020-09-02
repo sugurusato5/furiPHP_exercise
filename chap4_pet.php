801e3d7e4541c1e88e94a183e11c8a574b04f77b
sugurusato5 <jt.ag.dm.2782@gmai.com>
2020-09-03 00:21:05 +0900

    課題3提出

--- chap4_pet.php
+++ chap4_pet.php
@@ -1,7 +1,7 @@
 <?php
 function displayMsg($owner) {
     $animal_type = array_unique($owner["animal"]);
-    $animal = implode($animal_type);
+    $animal = implode(",",$animal_type);
     $cnt = count($owner["animal"]);
     $msg = <<< EOM
 {$owner["name"]}さんは
